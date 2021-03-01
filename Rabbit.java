442498e0c816de4fac2cf88f83067e1329a11948
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2021-03-01 04:22:52 -0300

    :wastebasket: Removendo os codigos da classe Rabbit

--- Rabbit.java
+++ Rabbit.java
@@ -8,7 +8,7 @@ import java.util.Random;
  * @author David J. Barnes and Michael Kolling
  * @version 2002-04-11
  */
-public class Rabbit
+public class Rabbit extends Animal
 {
     // Characteristics shared by all rabbits (static fields).
 
@@ -27,10 +27,6 @@ public class Rabbit
     
     // The rabbit's age.
     private int age;
-    // Whether the rabbit is alive or not.
-    private boolean alive;
-    // The rabbit's position
-    private Location location;
 
     /**
      * Create a new rabbit. A rabbit may be created with age
@@ -38,10 +34,10 @@ public class Rabbit
      * 
      * @param randomAge If true, the rabbit will have a random age.
      */
-    public Rabbit(boolean randomAge)
+    public Rabbit(boolean randomAge, Field field, Location location)
     {
+        super(field, location);
         age = 0;
-        alive = true;
         if(randomAge) {
             age = rand.nextInt(MAX_AGE);
         }
@@ -54,10 +50,10 @@ public class Rabbit
     public void run(Field updatedField, List newRabbits)
     {
         incrementAge();
-        if(alive) {
+        if(super.isAlive()) {
             int births = breed();
             for(int b = 0; b < births; b++) {
-                Rabbit newRabbit = new Rabbit(false);
+                Rabbit newRabbit = new Rabbit(false, field, location);
                 newRabbits.add(newRabbit);
                 Location loc = updatedField.randomAdjacentLocation(location);
                 newRabbit.setLocation(loc);
@@ -71,7 +67,7 @@ public class Rabbit
             }
             else {
                 // can neither move nor stay - overcrowding - all locations taken
-                alive = false;
+                super.setDead();
             }
         }
     }
@@ -84,7 +80,7 @@ public class Rabbit
     {
         age++;
         if(age > MAX_AGE) {
-            alive = false;
+            super.setDead();
         }
     }
     
@@ -110,39 +106,14 @@ public class Rabbit
         return age >= BREEDING_AGE;
     }
     
-    /**
-     * Check whether the rabbit is alive or not.
-     * @return True if the rabbit is still alive.
-     */
-    public boolean isAlive()
-    {
-        return alive;
-    }
 
     /**
      * Tell the rabbit that it's dead now :(
      */
     public void setEaten()
     {
-        alive = false;
+        super.setDead();
     }
     
-    /**
-     * Set the animal's location.
-     * @param row The vertical coordinate of the location.
-     * @param col The horizontal coordinate of the location.
-     */
-    public void setLocation(int row, int col)
-    {
-        this.location = new Location(row, col);
-    }
 
-    /**
-     * Set the rabbit's location.
-     * @param location The rabbit's location.
-     */
-    public void setLocation(Location location)
-    {
-        this.location = location;
-    }
 }

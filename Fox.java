7878ed0797a432fbb7a8227c539153ad2bc5e8a6
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2021-03-03 17:59:00 -0300

    :bulb: Comentarios

--- Fox.java
+++ Fox.java
@@ -17,7 +17,7 @@ public class Fox extends Animal {
     // The age to which a fox can live.
     private static final int MAX_AGE = 150;
     // The likelihood of a fox breeding.
-    private static final double BREEDING_PROBABILITY = 0.09;
+    private static final double BREEDING_PROBABILITY = 0.1;
     // The maximum number of births.
     private static final int MAX_LITTER_SIZE = 3;
     // The food value of a single rabbit. In effect, this is the

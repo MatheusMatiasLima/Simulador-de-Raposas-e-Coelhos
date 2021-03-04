351cfad06563a7c0157eba662724504fc145b580
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2021-03-04 04:49:20 -0300

    :bulb: comentando a classe Actor

--- Actor.java
+++ Actor.java
@@ -1,6 +1,8 @@
 import java.util.List;
 
 public interface Actor {
+    // ação do ator
     abstract public void act (Field currentField, Field updatedField, List newActors);
+    // se ele exite
     abstract public boolean isAtive();
 }

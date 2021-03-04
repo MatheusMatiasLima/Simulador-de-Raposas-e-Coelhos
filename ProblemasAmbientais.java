072ce3c9295686c70afdda2fc0e1d05fe6e17ece
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2021-03-04 04:35:31 -0300

    :sparkles: Criação da classe Problemas Ambientais

---
+++ ProblemasAmbientais.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Random;
+
+public abstract class ProblemasAmbientais implements Actor {
+
+    protected Location location;
+    private static final Random rand = new Random(); 
+
+    abstract public void act (Field currentField, Field updatedField, List newActors);
+
+    public void setLocation(int row, int col) {
+        this.location = new Location(row, col);
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}

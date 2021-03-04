c654a373264a6e7720646f92f2f75cbb93ecb95d
Matheus Matias <40282042+MatheusMatiasLima@users.noreply.github.com>
2021-03-04 04:39:54 -0300

    :pencil2: Renomeando parâmetros

--- Actor.java
+++ Actor.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
 public interface Actor {
-    abstract public void act (Field currentField, Field updatedField, List newFoxess);
+    abstract public void act (Field currentField, Field updatedField, List newActors);
     abstract public boolean isAtive();
 }
